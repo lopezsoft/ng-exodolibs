d1f1069319230ac1689b2ed384dd9a8fbac2e5f9
lopezsoft1 <lopezsoft.com@gmail.com>
2023-09-29 09:33:38 -0500

    refactor: Changes to global styles

--- projects/exodolibs/src/lib/pipes/safe-html.pipe.ts
+++ projects/exodolibs/src/lib/pipes/safe-html.pipe.ts
@@ -8,7 +8,7 @@ export class SafeHtmlPipe implements PipeTransform {
   constructor(
     private sanitizer:DomSanitizer
   ){}
-  transform(html) {
+  transform(html: string) {
     return this.sanitizer.bypassSecurityTrustHtml(html);
   }
 
